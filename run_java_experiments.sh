0e208e7b01aba2d9f39d0c81c3543cfd6f1afbfe
jschwalb11 <joey.schwalb@gmail.com>
2023-11-30 14:29:00 -0600

    reverted changes to time format

--- run_java_experiments.sh
+++ run_java_experiments.sh
@@ -10,50 +10,37 @@ fi
 
 echo "Runs = $runs"
 
-javac Java/java_experiments.java
-javac Java/Max.java
-javac Java/Min.java
-javac Java/Sum.java
-javac Java/Avg.java
-
 echo "BEGIN - Java Binary Search"
-{ time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" for i in $(seq 1 $runs); do
-    echo "Run $i"
-    java Java.java_experiments
-done; } 2>&1
+time (for i in $(seq 1 $runs); do
+  echo "Run $i"
+  java Java.java_experiments
+done) > java_search.txt
 echo "END - Java Binary Search"
 
-# echo "BEGIN - Java Binary Search"
-# { time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" for i in $(seq 1 $runs); do
-#   echo "Run $i"
-#   java Java.java_experiments
-# done; } 2>&1
-# echo "END - Java Binary Search"
+
+echo "BEGIN - Java Sum Pooling"
+time (for i in $(seq 1 $runs); do
+  java Java.Sum
+done) > java_sum.txt
+echo "END - Java Sum Pooling"
 
 
-# echo "BEGIN - Java Sum Pooling"
-# time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" (for i in $(seq 1 $runs); do
-#   java Java.Sum
-# done) > java_sum.txt
-# echo "END - Java Sum Pooling"
+echo "BEGIN - Java Min Pooling"
+time (for i in $(seq 1 $runs); do
+  java Java.Min
+done) > java_Min.txt
+echo "END - Java Min Pooling"
 
 
-# echo "BEGIN - Java Min Pooling"
-# time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" (for i in $(seq 1 $runs); do
-#   java Java.Min
-# done) > java_Min.txt
-# echo "END - Java Min Pooling"
+echo "BEGIN - Java Max Pooling"
+time (for i in $(seq 1 $runs); do
+  java Java.Max
+done) > java_Max.txt
+echo "END - Java Max Pooling"
 
 
-# echo "BEGIN - Java Max Pooling"
-# time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" (for i in $(seq 1 $runs); do
-#   java Java.Max
-# done) > java_Max.txt
-# echo "END - Java Max Pooling"
-
-
-# echo "BEGIN - Java Avg Pooling"
-# time --format="Command: %C\nUser time: %U\nSystem time: %S\nTotal time: %E\nCPU percentage: %P" (for i in $(seq 1 $runs); do
-#   java Java.Avg
-# done) > java_Avg.txt
-# echo "END - Java Avg Pooling"
+echo "BEGIN - Java Avg Pooling"
+time (for i in $(seq 1 $runs); do
+  java Java.Avg
+done) > java_Avg.txt
+echo "END - Java Avg Pooling"
