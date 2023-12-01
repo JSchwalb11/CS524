c4c3e19a9d87a6b60e62cb1e1bee678aa6b25093
jschwalb11 <joey.schwalb@gmail.com>
2023-12-01 02:03:21 -0600

    wow, i didnt commit this change

--- compile_experiments.sh
+++ compile_experiments.sh
@@ -3,9 +3,11 @@ javac Java/Max.java
 javac Java/Min.java
 javac Java/Sum.java
 javac Java/Avg.java
+javac Java/Linear.Java
 
 g++ --std=c++17 C++/cpp_experiments.cpp -o C++/cpp_experiments
 g++ C++/min.cpp -o C++/min
 g++ C++/max.cpp -o C++/max
 g++ C++/avg.cpp -o C++/avg
 g++ C++/sum.cpp -o C++/sum
+g++ C++/linear.cpp -o C++/linear
